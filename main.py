1bc3694203a767b438f54b3bfa3afcee43169673
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2021-03-20 22:07:54 +0300

    add POST /couriers

--- main.py
+++ main.py
@@ -1,6 +1,69 @@
-from flask import Flask
+from flask import Flask, request, abort, jsonify, Response
+from session import Session
 
 app = Flask(__name__)
+
+session = Session()
+
+
+def check_params(req, params):
+    id_couriers = []
+    dop = params
+    for courier in req:
+        fields = [False, False, False, False]
+        fl = True
+        for ind, field in enumerate(courier.keys()):
+            if field in dop:
+                fields[ind] = True
+            else:
+                id_couriers.append({"id": courier["courier_id"]})
+                fl = False
+                break
+        if fl:
+            if False in fields:
+                id_couriers.append({"id": courier["courier_id"]})
+    return id_couriers
+
+
+@app.route('/couriers', methods=["POST"])
+def couriers():
+    """
+    /couriers:
+        post:
+            description: 'Import couriers'
+            requestBody:
+                content:
+                    application/json:
+                        schema:
+                            $ref: '#/components/schemas/CouriersPostRequest'
+            responses:
+                '201':
+                    description: 'Created'
+                    content:
+                        application/json:
+                            schema:
+                                $ref: '#/components/schemas/CouriersIds'
+                '400':
+                    description: 'Bad request'
+                    content:
+                        application/json:
+                            schema:
+                                type: object
+                                additionalProperties: true
+                                properties:
+                                    validation_error:
+                                        $ref: '#/components/schemas/CouriersIds'
+                                required:
+                                  - validation_error
+    :return:
+    """
+    data = request.json['data']
+    id_couriers = check_params(data, ['courier_id', 'courier_type', 'regions', 'working_hours'])
+    if len(id_couriers) > 0:
+        return jsonify({"validation_error": {"couriers": id_couriers}}), 400
+    session.add_couriers(data)
+    return jsonify({"couriers": list({"id": courier["courier_id"]} for courier in data)}), 201
+
 
 if __name__ == '__main__':
     app.run(port=8080)
--- session.py
+++ session.py
@@ -7,14 +7,29 @@ class Session:
         db_session.global_init('db/db.db')
         self.session = db_session.create_session()
 
-    def insert_new_user(self, courier_id):
-        user = couriers.Couriers(id=courier_id)
-        self.session.add(user)
-        self.session.commit()
+    def add_courier(self, courier):
+        self.session.add(courier)
+
+    def add_couriers(self, couriers_data):
+        for courier in couriers_data:
+            current_courier = couriers.Couriers()
+            current_courier.id = courier["courier_id"]
+            current_courier.type = courier["courier_type"]
+            current_courier.regions = ';'.join(map(str, courier["regions"]))
+            current_courier.regions = ';'.join(courier["working_hours"])
+            self.session.add(current_courier)
+            self.commit()
 
     def get_courier(self, courier_id):
-        user = self.session.query(couriers.Couriers).filter(couriers.Couriers.id == courier_id).first()
-        return user
+        return self.session.query(couriers.Couriers).filter(
+            couriers.Couriers.id == courier_id).first()
+
+    def set_args_courier(self, courier_id, args):
+        current_courier = self.get_courier(courier_id)
+        for i in args.keys():
+            eval(f'current_courier.{i} = {";".join(map(str, args[i]))}')
+            self.commit()
+        return current_courier
 
     def commit(self):
         self.session.commit()
