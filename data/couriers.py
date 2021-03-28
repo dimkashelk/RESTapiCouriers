59dbb9d885f97396e91f063791833c5340cdbad0
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2021-03-29 02:53:21 +0300

    update default values in couriers.py

--- data/couriers.py
+++ data/couriers.py
@@ -1,5 +1,6 @@
 import sqlalchemy
 from .db_session import SqlAlchemyBase
+import json
 
 
 class Couriers(SqlAlchemyBase):
@@ -7,8 +8,8 @@ class Couriers(SqlAlchemyBase):
 
     id = sqlalchemy.Column(sqlalchemy.INT, primary_key=True)
     type = sqlalchemy.Column(sqlalchemy.VARCHAR, default='')
-    regions = sqlalchemy.Column(sqlalchemy.JSON)
-    working_hours = sqlalchemy.Column(sqlalchemy.JSON)
-    orders = sqlalchemy.Column(sqlalchemy.JSON)
+    regions = sqlalchemy.Column(sqlalchemy.JSON, default=json.dumps({}))
+    working_hours = sqlalchemy.Column(sqlalchemy.JSON, default=json.dumps({}))
+    orders = sqlalchemy.Column(sqlalchemy.JSON, default=json.dumps({}))
     earnings = sqlalchemy.Column(sqlalchemy.BIGINT, default=0)
     assign_time = sqlalchemy.Column(sqlalchemy.VARCHAR, default='')
